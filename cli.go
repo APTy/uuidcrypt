dfbb765d5fa07c5fdb0c2b9920a7d94a7cd68ccb
Tyler Julian <tjulian@uber.com>
2017-06-06 13:29:50 -0700

    Add in-place modification of files

--- cli.go
+++ cli.go
@@ -17,7 +17,16 @@ type flags struct {
 }
 
 func RunCLI() {
-	f := parseFlags()
+	if err := runCLI(); err != nil {
+		os.Stderr.WriteString(err.Error() + "\n")
+	}
+}
+
+func runCLI() error {
+	f, err := parseFlags()
+	if err != nil {
+		return err
+	}
 	uuidCrypt := NewUUIDCrypt(
 		NewCSVFile(f.inputFile),
 		NewCSVFile(f.outputFile),
@@ -25,11 +34,15 @@ func RunCLI() {
 		WithColumns(1),
 	)
 	if err := uuidCrypt.Run(); err != nil {
-		os.Stderr.WriteString(err.Error() + "\n")
+		return err
 	}
+	if err := removeBackupFileIfInPlace(f); err != nil {
+		return err
+	}
+	return nil
 }
 
-func parseFlags() flags {
+func parseFlags() (flags, error) {
 	f := defaultFlagsFromEnv()
 	stringVarIfNoDefault(&f.secret, "s", "Secret key used to generate all encryption keys")
 	stringVarIfNoDefault(&f.namespace, "n", "Namespace to generate an entity-specific encryption key")
@@ -37,13 +50,16 @@ func parseFlags() flags {
 	// flag.StringVar(&f.columns, "c", "", "Comma-separated list of columns to encrypt/decrypt (default: 1)")
 	flag.StringVar(&f.outputFile, "o", "-", "Output file")
 	flag.BoolVar(&f.decrypt, "d", false, "Set operation to DECRYPT (default: ENCRYPT)")
-	// flag.BoolVar(&f.inPlace, "i", false, "Operate on the file in-place")
+	flag.BoolVar(&f.inPlace, "i", false, "Operate on the file in-place")
 	flag.Parse()
 	f.inputFile = flag.Arg(0)
 	if f.inputFile == "" {
-		f.inputFile = "-"
+		f.inputFile = stdPipe
 	}
-	return f
+	if err := setFilesIfInPlace(&f); err != nil {
+		return f, err
+	}
+	return f, nil
 }
 
 func defaultFlagsFromEnv() flags {
@@ -71,4 +87,35 @@ func toCryptType(shouldDecrypt bool) CryptType {
 		return DecryptType
 	}
 	return EncryptType
+}
+
+func setFilesIfInPlace(f *flags) error {
+	if !f.inPlace {
+		return nil
+	}
+	backupFile, err := createBackupFile(f.inputFile)
+	if err != nil {
+		return err
+	}
+	f.outputFile = f.inputFile
+	f.inputFile = backupFile
+	return nil
+}
+
+func createBackupFile(filename string) (string, error) {
+	if filename == stdPipe {
+		return stdPipe, nil
+	}
+	newFilename := filename + ".tmp.uuidcrypt"
+	if err := os.Rename(filename, newFilename); err != nil {
+		return "", err
+	}
+	return newFilename, nil
+}
+
+func removeBackupFileIfInPlace(f flags) error {
+	if !f.inPlace || f.inputFile == stdPipe {
+		return nil
+	}
+	return os.Remove(f.inputFile)
 }
